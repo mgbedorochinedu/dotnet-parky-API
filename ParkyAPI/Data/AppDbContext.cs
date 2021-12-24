7a0b8be5bbefd165c8376305b6671547be9eba2b
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2021-12-24 18:19:21 +0100

    Add project files.

---
+++ ParkyAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
